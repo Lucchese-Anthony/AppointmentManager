67ce09d72fbe43c9f4d5063e1643290512800930
Lucchese-Anthony <anthony-lucchese@outlook.com>
2022-03-23 18:06:54 -0400

    editing setip

--- src/setup.py
+++ src/setup.py
@@ -6,32 +6,18 @@ def main():
     if not os.path.isdir("database"):
         os.mkdir("database")
     os.chdir(f"{os.getcwd()}/database/")
-    query = 
-    """CREATE TABLE APPOINTMENTS IF NOT EXISTS(
+    query = """CREATE TABLE IF NOT EXISTS APPOINTMENTS(
         APPID           INT PRIMARY KEY     NOT NULL,
         USERID           TEXT                NOT NULL,
         TOPIC            TEXT                NOT NULL,
-        EMAIL            TEXT                NOT NULL,
-        
+        EMAIL            TEXT                NOT NULL
         );"""
     conn = sqlite3.connect('appointments.db')
-    otherInputs = raw_input("would you like to add other attributes to the table? (yes/no)> ")
-    while(otherInputs.lower() != "no"):
-        Name = raw_input("What would you like the row value be?> ")
-        Type = raw_input("What would you like the row type be? (INT / TEXT / REAL/ NONE)> ")
-        nullable = raw_input("is it (NULL) or (NOT NULL)> ")
-        if nullable == "NULL":
-            nullable = ""
-        query = query + f" {Name} {Type} {nullable}"
-        otherInputs = raw_input("would you like to add other attributes to the table? (yes/no)> ")
-        
-    conn.execute('''CREATE TABLE APPOINTMENTS IF NOT EXISTS
-        (APPID           INT PRIMARY KEY     NOT NULL,
-        USERID           TEXT                NOT NULL,
-        TOPIC            TEXT                NOT NULL,
-        EMAIL            TEXT                NOT NULL,
-        );''')
-
+    otherInputs = str(input("The default table contains:\n- Appointment ID\n- User ID\n- Meeting Topic\n- email address\n Is that ok? (y/n)> "))
+    if otherInputs == "n":
+        print("exiting program")
+        exit()
+    conn.execute(query)
 
 
 if __name__ == "__main__":
