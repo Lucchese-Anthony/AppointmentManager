4cf46a1a165b04520a5ec331820554b906caca56
Lucchese-Anthony <anthony-lucchese@outlook.com>
2022-03-23 17:38:46 -0400

    setup.py

---
+++ src/setup.py
@@ -0,0 +1,38 @@
+import sqlite3
+import os
+
+def main():
+    path = os.getcwd()
+    if not os.path.isdir("database"):
+        os.mkdir("database")
+    os.chdir(f"{os.getcwd()}/database/")
+    query = 
+    """CREATE TABLE APPOINTMENTS IF NOT EXISTS(
+        APPID           INT PRIMARY KEY     NOT NULL,
+        USERID           TEXT                NOT NULL,
+        TOPIC            TEXT                NOT NULL,
+        EMAIL            TEXT                NOT NULL,
+        
+        );"""
+    conn = sqlite3.connect('appointments.db')
+    otherInputs = raw_input("would you like to add other attributes to the table? (yes/no)> ")
+    while(otherInputs.lower() != "no"):
+        Name = raw_input("What would you like the row value be?> ")
+        Type = raw_input("What would you like the row type be? (INT / TEXT / REAL/ NONE)> ")
+        nullable = raw_input("is it (NULL) or (NOT NULL)> ")
+        if nullable == "NULL":
+            nullable = ""
+        query = query + f" {Name} {Type} {nullable}"
+        otherInputs = raw_input("would you like to add other attributes to the table? (yes/no)> ")
+        
+    conn.execute('''CREATE TABLE APPOINTMENTS IF NOT EXISTS
+        (APPID           INT PRIMARY KEY     NOT NULL,
+        USERID           TEXT                NOT NULL,
+        TOPIC            TEXT                NOT NULL,
+        EMAIL            TEXT                NOT NULL,
+        );''')
+
+
+
+if __name__ == "__main__":
+    main()
